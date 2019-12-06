67d0d8d33f09e161e2891588d530fa094f6af048
Jack Smith <jack.smith@elucidata.co.uk>
2019-12-06 18:37:24 +0000

    De-duplicate loader test case

--- tests/test_loader.py
+++ tests/test_loader.py
@@ -6,20 +6,28 @@ from json_ref_dict import RefDict
 from json_ref_dict.exceptions import DocumentParseError
 
 
+PINNED_FILE_URL = (
+    "https://raw.githubusercontent.com/jacksmith15/json-ref-dict/091af2"
+    "c19989a95449df587b62abea89aeb83676/tests/schemas/master.yaml"
+)
+
+
 class TestRefDictFileSystemRefs:
     @staticmethod
     @pytest.fixture(
         scope="class",
         params=[
-            # relative
+            # relative filepath
             "tests/schemas/master.yaml#/definitions",
-            # absolute
+            # absolute filepath
             path.join(getcwd(), "tests/schemas/master.yaml#/definitions"),
-            # explicit
+            # explicit file scheme
             (
                 "file://"
                 + path.join(getcwd(), "tests/schemas/master.yaml#/definitions")
             ),
+            # https URI
+            PINNED_FILE_URL + "#/definitions",
         ],
     )
     def schema(request):
@@ -61,48 +69,3 @@ class TestRefDictFileSystemRefs:
     def test_loading_unknown_file_raises_document_parse_error():
         with pytest.raises(DocumentParseError):
             _ = RefDict("tests/schemas/nonexistent.yaml#/definitions")
-
-
-@pytest.mark.foo
-class TestRefDictHTTPefs:
-    @staticmethod
-    @pytest.fixture(scope="module")
-    def schema():
-        # Permalink to file url on Github.
-        file_url = (
-            "https://raw.githubusercontent.com/jacksmith15/json-ref-dict/091af2"
-            "c19989a95449df587b62abea89aeb83676/tests/schemas/master.yaml"
-        )
-        return RefDict(f"{file_url}#/definitions")
-
-    @staticmethod
-    def test_schema_loads(schema):
-        assert str(schema) == str(
-            {
-                "foo": {"type": "string"},
-                "local_ref": {"$ref": "#/definitions/foo"},
-                "remote_ref": {"$ref": "other.yaml#/definitions/bar"},
-                "backref": {"$ref": "other.yaml#/definitions/baz"},
-            }
-        )
-
-    @staticmethod
-    def test_local_ref(schema):
-        assert schema["local_ref"] == {"type": "string"}
-
-    @staticmethod
-    def test_remote_ref(schema):
-        assert schema["remote_ref"] == {"type": "integer"}
-
-    @staticmethod
-    def test_backref(schema):
-        assert schema["backref"] == {"type": "string"}
-
-    @staticmethod
-    def test_casting_to_dict_dereferences_all(schema):
-        assert dict(schema) == {
-            "foo": {"type": "string"},
-            "local_ref": {"type": "string"},
-            "remote_ref": {"type": "integer"},
-            "backref": {"type": "string"},
-        }
